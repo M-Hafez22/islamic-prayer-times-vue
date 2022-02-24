1fc939f8945582018b7b1f9972f08a01c5872ed8
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-25 01:25:10 +0200

    Make the app fetch after getting the location

--- src/components/prayerTimes/PrayTimes.vue
+++ src/components/prayerTimes/PrayTimes.vue
@@ -1,100 +1,91 @@
-<template>
-  <h1>Prayer Times</h1>
-  <div v-if="loading">
-    <h1>Loading...</h1>
-  </div>
-  <HijriDate v-if="!loading" :hijri="hijri" :gregorian="gregorian" />
-  <ul class="prayList" v-if="!loading">
-    <PrayCard
-      v-for="pray in praysNames"
-      :key="pray"
-      :name="pray"
-      :time="timings[pray].slice(0, 5)"
-    />
-  </ul>
-  <RemaineTime
-    v-if="!loading"
-    :praysNames="praysNames"
-    :praysTimes="praysTimes"
-  />
-  <p>Time Zone is {{ timeZone }}</p>
-</template>
-
-<script>
+<script setup>
+import { ref, watch } from "vue";
 import PrayCard from "./PrayCard.vue";
 import RemaineTime from "./RemaineTime.vue";
 import HijriDate from "./HijriDate.vue";
-import { mapState } from "vuex";
 
-export default {
-  name: "PrayTimes",
-  data() {
-    return {
-      Loading: true,
-      data: {},
-      timings: {},
-      praysTimes: [],
-      praysNames: [],
-      latitude: "51.508515",
-      longitude: "-0.1254872",
-      gregorian: {},
-      hijri: {},
-      remaineTime: "",
-      nextPray: 1,
-      date: Math.floor(Date.now() / 1000),
-      timeZone: "",
-    };
-  },
-  components: {
-    HijriDate,
-    PrayCard,
-    RemaineTime,
-  },
-  mounted() {
-    setInterval(() => this.setTime(), 1000);
-  },
-  methods: {
-    setTime() {
-      this.date = new Date();
-    },
-  },
-  computed: {
-    ...mapState(["theme"]),
-  },
-  async created() {
-    // Get Current Position
-    const success = (position) => {
-      this.latitude = position.coords.latitude;
-      this.longitude = position.coords.longitude;
-    };
-    // Print error
-    const error = (position) => {
-      console.log(position.message);
-    };
-    // check Browser for navigator
-    !navigator.geolocation
-      ? console.log("Geolocation is not supported by your browser")
-      : navigator.geolocation.getCurrentPosition(success, error);
+// Initial URl
+const latitude = ref("51.508515");
+const longitude = ref("-0.1254872");
+const date = ref(Math.floor(Date.now() / 1000));
+const apiUrl = ref(
+  `https://api.aladhan.com/v1/timings/${date.value}?latitude=${latitude.value}&longitude=${longitude.value}&method=5`
+);
 
-    // GET request using fetch with async/await
-    const response = await fetch(
-      `https://api.aladhan.com/v1/timings/${this.date}?latitude=${this.latitude}&longitude=${this.longitude}&method=5`
-    );
-    const data = await response.json();
-    this.data = data.data;
-    this.Loading = false;
+const loaded = ref(null);
+const timings = ref(null);
+const praysNames = ref(null);
+const praysTimes = ref(null);
+const gregorian = ref(null);
+const hijri = ref(null);
+const timeZone = ref(null);
 
-    // additional data to be removed
-    const additional = ["Sunrise", "Imsak", "Midnight", "Sunset"];
-    // Remove additional data
-    additional.forEach((i) => delete data.data.timings[i]);
-    // Set Local states
-    this.timings = data.data.timings;
-    this.praysNames = Object.keys(data.data.timings);
-    this.praysTimes = Object.values(data.data.timings);
-    this.gregorian = data.data.date.gregorian;
-    this.hijri = data.data.date.hijri;
-    this.timeZone = data.data.meta.timezone;
-  },
+// Update URL with the Current Loaction
+// Get Current Position
+const success = (position) => {
+  latitude.value = position.coords.latitude;
+  longitude.value = position.coords.longitude;
+  apiUrl.value = `https://api.aladhan.com/v1/timings/${date.value}?latitude=${latitude.value}&longitude=${longitude.value}&method=5`;
 };
+// Print error
+const error = (position) => {
+  console.log(position.message);
+};
+// check Browser for navigator
+!navigator.geolocation
+  ? console.log("Geolocation is not supported by your browser")
+  : navigator.geolocation.getCurrentPosition(success, error);
+
+// API content holder
+const prayData = ref(null);
+
+// Fetching API
+async function fetchData() {
+  prayData.value = null;
+  const res = await fetch(apiUrl.value);
+  const data = await res.json();
+  prayData.value = data.data;
+  loaded.value = true;
+
+  // additional data to be removed
+  const additional = ["Sunrise", "Imsak", "Midnight", "Sunset"];
+  // Remove additional data
+  additional.forEach((i) => delete data.data.timings[i]);
+  // Set Local states
+  timings.value = prayData.value.timings;
+  praysNames.value = Object.keys(prayData.value.timings);
+  praysTimes.value = Object.values(prayData.value.timings);
+  gregorian.value = prayData.value.date.gregorian;
+  hijri.value = prayData.value.date.hijri;
+  timeZone.value = prayData.value.meta.timezone;
+}
+
+fetchData();
+
+watch(apiUrl, fetchData);
 </script>
+
+<template>
+  <div class="prayTime">
+    <div v-if="!loaded">
+      <h1>Loading...</h1>
+    </div>
+    <h1>{{ theme }}</h1>
+
+    <HijriDate v-if="loaded" :hijri="hijri" :gregorian="gregorian" />
+    <ul class="prayList" v-if="loaded">
+      <PrayCard
+        v-for="pray in praysNames"
+        :key="pray"
+        :name="pray"
+        :time="timings[pray].slice(0, 5)"
+      />
+    </ul>
+    <RemaineTime
+      v-if="loaded"
+      :praysNames="praysNames"
+      :praysTimes="praysTimes"
+    />
+  </div>
+  <h3>{{ timeZone }}</h3>
+</template>
