4e8617b1d45c9109625d91ff94de163e379c3bb3
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-24 23:32:21 +0200

    Add global states

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    theme: "dark",
+    lang: "en",
+    prayNames_ar: ["الفجر", "الظهر", "العصر", "المغرب", "العشاء"],
   },
   getters: {
   },
   mutations: {
+    changeTheme(state, payload) {
+      state.theme = payload
+    },
+    changeLang(state, payload) {
+      state.lang = payload;
+    },
   },
   actions: {
   },
