602d67ecc98296cccd946d47f7506004d69c672d
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-24 23:43:33 +0200

    Add Hijri Date

--- src/components/prayerTimes/HijriDate.vue
+++ src/components/prayerTimes/HijriDate.vue
@@ -1,3 +1,20 @@
 <template>
-    <h2>HijriDate</h2>
+  <h2>{{translatedDate}}</h2>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "HijriDate",
+  props: ["hijri", "gregorian"],
+  computed: {
+    ...mapState(["lang"]),
+    translatedDate() {
+      return this.lang === "en"
+        ? `${this.gregorian.weekday.en} ${this.hijri.day} ${this.hijri.month.en} ${this.hijri.year}`
+        : `${this.hijri.weekday.ar} ${this.hijri.day} ${this.hijri.month.ar} ${this.hijri.year}`;
+    },
+  },
+};
+</script>
