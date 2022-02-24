8cb73e3c702930696a2340ff00ad846e845c590a
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-25 01:51:35 +0200

    Enable changing theme

--- src/components/footer/appSettings/ChangeTheme.vue
+++ src/components/footer/appSettings/ChangeTheme.vue
@@ -1,3 +1,32 @@
 <template>
-    <h2>Make Change Theme</h2>
+  <div :class="theme === 'dark' ? 'optionsDark' : 'optionsLight'">
+    <button
+      class="option"
+      v-for="t in themes"
+      :key="t"
+      :class="[t == theme ? 'active' : '', 'option']"
+      @click="changeTheme(t)"
+    >
+      {{ t }}
+    </button>
+  </div>
 </template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "ChangeTheme",
+  data() {
+    return {
+      themes: ["dark", "light"],
+    };
+  },
+  computed: {
+    ...mapState(["theme", "lang"]),
+  },
+  methods: {
+    ...mapMutations(["changeTheme"]),
+  },
+};
+</script>
