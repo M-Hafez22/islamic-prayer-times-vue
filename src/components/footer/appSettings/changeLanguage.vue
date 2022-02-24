47cdfca5a17e96e6de20026df52fa6154dce1796
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-25 01:53:56 +0200

    Highlight active language

--- src/components/footer/appSettings/changeLanguage.vue
+++ src/components/footer/appSettings/changeLanguage.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div :class="theme === 'dark' ? 'optionsDark' : 'optionsLight'">
     <button
       class="option"
       v-for="language in languages"
       :key="language"
+      :class="[language == lang ? 'active' : '', 'option']"
       @click="changeLang(language)"
     >
       {{ language }}
