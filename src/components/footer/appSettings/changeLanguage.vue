53f3e60191ede8a341cd1918862d8f48e6f5ea89
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-02-25 01:44:47 +0200

    Enable changing lang

--- src/components/footer/appSettings/changeLanguage.vue
+++ src/components/footer/appSettings/changeLanguage.vue
@@ -1,3 +1,31 @@
 <template>
-    <h2>Make Change Language</h2>
+  <div>
+    <button
+      class="option"
+      v-for="language in languages"
+      :key="language"
+      @click="changeLang(language)"
+    >
+      {{ language }}
+    </button>
+  </div>
 </template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "changeLanguage",
+  data() {
+    return {
+      languages: ["ar", "en"],
+    };
+  },
+  methods: {
+    ...mapMutations(["changeLang"]),
+  },
+  computed: {
+    ...mapState(["theme", "lang"]),
+  },
+};
+</script>
